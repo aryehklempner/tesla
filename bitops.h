661361d52f1e1185d4ba4eaf36b09cddafdbdc3f
Aryeh Klempner <arayaydev@gmail.com>
2020-07-27 11:26:52 -0700

    Add include guard to bitops.h

--- bitops.h
+++ bitops.h
@@ -1,6 +1,8 @@
 /* Written by Aryeh Klempner <arayaydev@gmail.com>, 2020
 The current contents of this file are not copyrightable on grounds of triviality and originality.
 */
+#ifndef TESLA_BITOPS_H
+#define TESLA_BITOPS_H
 #include <stdint.h>
 
 static inline uint64_t rol(const uint64_t a, int x) {
@@ -10,3 +12,4 @@ static inline uint64_t rol(const uint64_t a, int x) {
 static inline uint64_t ror(const uint64_t a, int x) {
 	return (a >> x) | (a << (64 - x));
 }
+#endif
